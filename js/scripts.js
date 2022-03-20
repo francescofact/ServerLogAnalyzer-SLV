5fb703f404ed45e7556f68bb32316c3adeac4ef2
Francesco Fattori <ffattori@me.com>
2022-03-20 12:48:20 +0100

    added cities dot on globe

--- js/scripts.js
+++ js/scripts.js
@@ -51,7 +51,9 @@ am5.ready(function() {
     heatLegend.set("startValue", 0);
 
     am5.net.load("world.json", chart).then(function (result) {
-        var geodata = am5.JSONParser.parse(result.response);
+        let alldata = am5.JSONParser.parse(result.response);
+        //countries
+        let geodata = alldata["countries"];
         Object.entries(geodata).forEach(function(kv){
             polygonSeries.getDataItemById(kv[0]).set("value", kv[1])
         });
@@ -65,7 +67,32 @@ am5.ready(function() {
             key: "fill"
         }]);
         heatLegend.set("endValue", maxGlobe);
-        console.log(maxGlobe);
+        
+        //citites
+        let cities = alldata["cities"];
+        var pointSeries = chart.series.push(am5map.MapPointSeries.new(root, {}));
+        pointSeries.bullets.push(function () {
+            var container = am5.Container.new(root, {});
+            
+            container.children.push(
+                am5.Circle.new(root, {
+                radius: 2,
+                fill: "#db2d21",
+                strokeOpacity: 0
+                })
+            );
+            
+            return am5.Bullet.new(root, {
+                sprite: container
+            });
+        });
+
+        for (var i = 0; i < cities.length; i++) {
+            var city = cities[i];
+            pointSeries.data.push({
+                geometry: { type: "Point", coordinates: [city[1], city[0]] }
+            });
+        }
     });
     
     polygonSeries.mapPolygons.template.setAll({
